4e2a74eba73fbe0e28cd4e21f1ba2ec8a552e70e
ngyahloon <ngyahloon@43b3cfb2-0554-0410-942f-c3ce84cb332f>
2008-08-28 19:30:38 +0000

    Added MainApp which also runs the axis2server

--- axis2swing2/src/axis2swing/middleware/Axis2AdminManager.java
+++ axis2swing2/src/axis2swing/middleware/Axis2AdminManager.java
@@ -3,17 +3,25 @@ package axis2swing.middleware;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
+import org.apache.axis2.engine.AxisServer;
+import org.apache.axis2.transport.SimpleAxis2Server;
 
 public class Axis2AdminManager {
 	
-	public Axis2AdminManager () {
-		try {
-			ConfigurationContext myConfigContext = 
-			     ConfigurationContextFactory.createConfigurationContextFromFileSystem("/usr/local/apache-tomcat-6.0.16/webapps/axis2/WEB-INF" , "/usr/local/apache-tomcat-6.0.16/webapps/axis2/WEB-INF/conf/axis2.xml");
-		} catch (AxisFault e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	private ConfigurationContext configContext;
+	
+	public ConfigurationContext getConfigContext() {
+		return configContext;
+	}
+
+	public void setConfigContext(ConfigurationContext configContext) {
+		this.configContext = configContext;
+	}
+
+	public Axis2AdminManager(ConfigurationContext configContext) {
+		
+		this.configContext = configContext;
+		
 	}
 
 }
---
+++ axis2swing2/src/axis2swing/ui/MainApp.java
@@ -0,0 +1,84 @@
+package axis2swing.ui;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.axis2.context.ConfigurationContextFactory;
+import org.apache.axis2.engine.AxisServer;
+import org.apache.axis2.transport.http.SimpleHTTPServer;
+import org.apache.axis2.util.CommandLineOption;
+import org.apache.axis2.util.CommandLineOptionParser;
+import org.apache.axis2.util.OptionsValidator;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MainApp extends AxisServer {
+	
+	private static final Log log = LogFactory.getLog(SimpleHTTPServer.class);
+
+    int port = -1;
+
+    public static int DEFAULT_PORT = 8080;
+
+    public MainApp (
+            String repoLocation,
+            String confLocation) throws Exception {
+        super(false);
+        configContext = ConfigurationContextFactory
+                .createConfigurationContextFromFileSystem(repoLocation,
+                        confLocation);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        String repoLocation = "axis2-1.4/repository";
+        String confLocation = "axis2-1.4/conf/axis2.xml";
+
+        /*CommandLineOptionParser optionsParser = new CommandLineOptionParser(args);
+        List invalidOptionsList = optionsParser.getInvalidOptions(new OptionsValidator() {
+            public boolean isInvalid(CommandLineOption option) {
+                String optionType = option.getOptionType();
+                return !("repo".equalsIgnoreCase(optionType) || "conf"
+                        .equalsIgnoreCase(optionType));
+            }
+        });
+
+        if ((invalidOptionsList.size() > 0) || (args.length > 4)) {
+            printUsage();
+            return;
+        }
+
+        Map optionsMap = optionsParser.getAllOptions();
+
+        CommandLineOption repoOption = (CommandLineOption) optionsMap
+                .get("repo");
+        CommandLineOption confOption = (CommandLineOption) optionsMap
+                .get("conf");
+
+        log.info("[SimpleAxisServer] Starting");
+        if (repoOption != null) {
+            repoLocation = repoOption.getOptionValue();
+            log.info("[SimpleAxisServer] Using the Axis2 Repository"
+                    + new File(repoLocation).getAbsolutePath());
+            System.out.println("[SimpleAxisServer] Using the Axis2 Repository"
+                    + new File(repoLocation).getAbsolutePath());
+        }
+        if (confOption != null) {
+            confLocation = confOption.getOptionValue();
+            System.out
+                    .println("[SimpleAxisServer] Using the Axis2 Configuration File"
+                            + new File(confLocation).getAbsolutePath());
+        }*/
+
+        try {
+            MainApp server = new MainApp(repoLocation, confLocation);
+            server.start();
+            log.info("[MainApp] Started");
+            System.out.println("[MainApp] Started");
+        } catch (Throwable t) {
+            log.fatal("[MainApp] Shutting down. Error starting MainApp", t);
+            System.err.println("[MainApp] Shutting down. Error starting MainApp");
+        }
+    }
+
+}
