42d3bb09e2b368477a3566ece5777e2210af8a26
Vina.Wibowo <Vina.Wibowo@43b3cfb2-0554-0410-942f-c3ce84cb332f>
2008-09-04 16:11:16 +0000

    modifying Service class

--- trunk/axis2swing2/src/axis2swing/data/Service.java
+++ trunk/axis2swing2/src/axis2swing/data/Service.java
@@ -3,68 +3,87 @@ package axis2swing.data;
 import java.util.LinkedList;
 import java.util.List;
 
-
-public class Service {
+public class Service
+{
 	private String name;
-	private String EPR;
+	private String epr;
 	private String description;
 	private boolean isActive;
-	private List<Module> engagedMod;
-	private List<Operation> availableOp;
-	
-	public Service(String name, String EPR, boolean isActive) {
+	private List<Module> lstModule;
+	private List<Operation> lstOperation;
+
+	public Service(String name, String epr, boolean isActive)
+	{
 		this.name = name;
-		this.EPR = EPR;
-		this.description = "description";
+		this.epr = epr;
 		this.isActive = isActive;
-		this.engagedMod = null;
-		this.availableOp = null;
+		this.description = name;
 	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setEPR(String EPR) {
-		this.EPR = EPR;
-	}
-	public String getEPR() {
-		return EPR;
-	}
-	public void setStatus(boolean isActive) {
-		this.isActive = isActive;
-	}
-	public boolean getStatus() {
-		return isActive;
-	}
-	
-	public void addEngagedMod(Module engagedMod) {
-		if(this.engagedMod == null)
-			this.engagedMod = new LinkedList<Module>();
-		
-		this.engagedMod.add(engagedMod);
-	}
-	
-	public void setEngagedMod(List<Module> engagedMod) {
-		this.engagedMod = engagedMod;
-	}
-	public List<Module> getEngagedMod() {
-		return engagedMod;
-	}
-	public void setAvailableOp(List<Operation> availableOp) {
-		this.availableOp = availableOp;
-	}
-	public List<Operation> getAvailableOp() {
-		return availableOp;
-	}
-	public void setDescription(String description) {
+
+	public Service(String name, String epr, boolean isActive, String description)
+	{
+		this(name, epr, isActive);
 		this.description = description;
 	}
-	public String getDescription() {
-		return description;
+
+	public String getName()
+	{
+		return this.name;
 	}
-	
-	
+
+	public String getEpr()
+	{
+		return this.epr;
+	}
+
+	public String getDescription()
+	{
+		return this.description;
+	}
+
+	public boolean isActive()
+	{
+		return this.isActive;
+	}
+
+	public void setStatus(boolean isActive)
+	{
+		this.isActive = isActive;
+	}
+
+	public void addModule(Module module)
+	{
+		if (lstModule == null)
+			lstModule = new LinkedList<Module>();
+
+		lstModule.add(module);
+	}
+
+	public void setModules(List<Module> lstModule)
+	{
+		this.lstModule = lstModule;
+	}
+
+	public List<Module> getModules()
+	{
+		return this.lstModule;
+	}
+
+	public void addOperation(Operation operation)
+	{
+		if (lstOperation == null)
+			lstOperation = new LinkedList<Operation>();
+
+		lstOperation.add(operation);
+	}
+
+	public void setOperations(List<Operation> lstOperation)
+	{
+		this.lstOperation = lstOperation;
+	}
+
+	public List<Operation> getOperations()
+	{
+		return this.lstOperation;
+	}
 }
