6a949dc9900a787e941cccd2e9650079180625bc
Vina.Wibowo <Vina.Wibowo@43b3cfb2-0554-0410-942f-c3ce84cb332f>
2008-09-04 16:04:02 +0000

    add Handler class

---
+++ axis2swing2/src/axis2swing/data/Handler.java
@@ -0,0 +1,16 @@
+package axis2swing.data;
+
+public class Handler
+{
+	private String name;
+
+	public Handler(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+}
