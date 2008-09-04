2bce507a48af62dbadeb383abff38df3a3e17655
Vina.Wibowo <Vina.Wibowo@43b3cfb2-0554-0410-942f-c3ce84cb332f>
2008-09-04 16:12:22 +0000

    modifying ServiceGroup

--- axis2swing2/src/axis2swing/data/ServiceGroup.java
+++ axis2swing2/src/axis2swing/data/ServiceGroup.java
@@ -3,50 +3,55 @@ package axis2swing.data;
 import java.util.LinkedList;
 import java.util.List;
 
-
-public class ServiceGroup {
+public class ServiceGroup
+{
 	private String name;
 	private List<Service> lstService;
-	private List<Module> lstModules;
-	
-	public ServiceGroup(String name) {
+	private List<Module> lstModule;
+
+	public ServiceGroup(String name)
+	{
 		this.name = name;
 	}
-	
-	public void setName(String name) {
-		this.name = name;
+
+	public String getName()
+	{
+		return this.name;
 	}
-	public String getName() {
-		return name;
-	}
-	
-	public void addService(Service service) {
-		if(lstService == null)
+
+	public void addService(Service service)
+	{
+		if (lstService == null)
 			lstService = new LinkedList<Service>();
-		
+
 		lstService.add(service);
 	}
-	
-	public void setServices(List<Service> lstService) {
+
+	public void setServices(List<Service> lstService)
+	{
 		this.lstService = lstService;
 	}
-	public List<Service> getServices() {
-		return lstService;
+
+	public List<Service> getServices()
+	{
+		return this.lstService;
 	}
-	
-	public void addModule(Module module) {
-		if(lstModules == null)
-			lstModules = new LinkedList<Module>();
-		
-		lstModules.add(module);
+
+	public void addModule(Module module)
+	{
+		if (lstModule == null)
+			lstModule = new LinkedList<Module>();
+
+		lstModule.add(module);
 	}
-	
-	public void setModules(List<Module> lstModules) {
-		this.lstModules = lstModules;
+
+	public void setModules(List<Module> lstModule)
+	{
+		this.lstModule = lstModule;
 	}
-	public List<Module> getModules() {
-		return lstModules;
+
+	public List<Module> getModules()
+	{
+		return this.lstModule;
 	}
-	
-	
 }
