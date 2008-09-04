6a9e2152a6c88eeafbd65ed583fa66cf919876e9
Vina.Wibowo <Vina.Wibowo@43b3cfb2-0554-0410-942f-c3ce84cb332f>
2008-09-04 16:05:18 +0000

    modify Module class

--- trunk/axis2swing2/src/axis2swing/data/Module.java
+++ trunk/axis2swing2/src/axis2swing/data/Module.java
@@ -1,32 +1,29 @@
 package axis2swing.data;
 
-public class Module {
+public class Module
+{
 	private String name;
 	private String description;
-	
-	public Module(String name) {
+
+	public Module(String name)
+	{
 		this.name = name;
 		this.description = "module description not found";
 	}
-	
-	public Module(String name, String description) {
+
+	public Module(String name, String description)
+	{
 		this.name = name;
-		if(description.equals(""))
-			description = "module description not found";
-		else
-			this.description = description;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getDescription() {
-		return description;
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public String getDescription()
+	{
+		return this.description;
 	}
 }
