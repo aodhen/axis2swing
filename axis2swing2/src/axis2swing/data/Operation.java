f7d057b3d5eebc88f518db4dec8c7076aac0d745
Vina.Wibowo <Vina.Wibowo@43b3cfb2-0554-0410-942f-c3ce84cb332f>
2008-09-04 16:06:29 +0000

    complete Operation class

--- axis2swing2/src/axis2swing/data/Operation.java
+++ axis2swing2/src/axis2swing/data/Operation.java
@@ -1,5 +1,38 @@
 package axis2swing.data;
 
-public class Operation {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Operation
+{
+	private String name;
+	private List<Module> lstModule;
+
+	public Operation(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void addModule(Module module)
+	{
+		if (lstModule == null)
+			lstModule = new LinkedList<Module>();
+
+		lstModule.add(module);
+	}
+
+	public void setModules(List<Module> lstModule)
+	{
+		this.lstModule = lstModule;
+	}
+
+	public List<Module> getModules()
+	{
+		return this.lstModule;
+	}
 }
