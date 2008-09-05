08bf598a82065205b6c2b2037fad28020facec4c
Vina.Wibowo <Vina.Wibowo@43b3cfb2-0554-0410-942f-c3ce84cb332f>
2008-09-05 16:58:59 +0000

    correct panel

--- axis2swing2/src/axis2swing/ui/content/AvailableModulesPanel.java
+++ axis2swing2/src/axis2swing/ui/content/AvailableModulesPanel.java
@@ -1,26 +1,51 @@
 package axis2swing.ui.content;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.BoxLayout;
-import javax.swing.ComboBoxModel;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 
-import axis2swing.data.Service;
+import axis2swing.data.Module;
 import axis2swing.ui.Axis2SwingController;
 
-public class AvailableModulesPanel 
+public class AvailableModulesPanel extends PanelContent
 {
-	
+	private static final long serialVersionUID = 1L;
+
+	public AvailableModulesPanel(Axis2SwingController controller)
+	{
+		super(controller);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initGUI()
+	{
+		List<Module> lstModule = controller.getAvailableModules();
+
+		if (lstModule == null || (lstModule != null && lstModule.isEmpty()))
+		{
+			setHeader("No available module");
+		}
+		else
+		{
+			setHeader("Available Modules");
+			setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+			String message = "<html><ul>";
+
+			for (int i = 0; i < lstModule.size(); i++)
+			{
+				Module module = lstModule.get(i);
+				message += "<li><b>" + module.getName() + "</b>: "
+						+ module.getDescription() + "</li>";
+			}
+
+			message += "</ul></html>";
+
+			JLabel newLabel = new JLabel(message);
+			add(newLabel);
+
+		}
+	}
 }
